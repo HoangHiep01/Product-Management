0d1fc391b2cc833e90a4c26bd9f2855751b6b384
HoangHiep <nhoanghiep2001@gmail.com>
2023-08-05 02:21:16 +0700

    changed websockets

--- mysite/Product/static/script/server.js
+++ mysite/Product/static/script/server.js
@@ -1,8 +1,18 @@
-window.addEventListener("DOMContentLoaded", () => {
 
-  const websocket = new WebSocket("ws://192.168.1.18:5678/");
-  websocket.onmessage = ({ data }) => {
-    document.getElementById("idInput").value = data;
-    document.getElementById('scanForm').submit();
-  };
+const websocket = new WebSocket("ws://192.168.1.18:5678/");
+
+websocket.onopen = function() {
+  alert('Connected!')
+};
+
+connection.addEventListener('message', function(event) {
+
+websocket.onmessage = function(data) {
+  document.getElementById("idInput").value = data;
+  document.getElementById('scanForm').submit();
+};
+
 });
+// eventName : open, close, send, message...
+
+
