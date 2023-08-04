7be4990da8c8af2baa3b221a9ad366d570030922
HoangHiep <nhoanghiep2001@gmail.com>
2023-08-04 16:04:29 +0700

    no login longer

--- mysite/Product/views.py
+++ mysite/Product/views.py
@@ -8,8 +8,8 @@ import socket
 # Create your views here.
 
 def home(request):
-	if request.method == 'POST':
-		login_user(request)
+	# if request.method == 'POST':
+	# 	login_user(request)
 	
 	return render(request, 'product/home.html', {})
 
