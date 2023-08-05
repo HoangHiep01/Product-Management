b0564c3182dd0458112f43f4ca4c1704365100fd
HoangHiep <nhoanghiep2001@gmail.com>
2023-08-05 21:03:40 +0700

    Update websocket

--- mysite/Product/static/script/server.js
+++ mysite/Product/static/script/server.js
@@ -1,18 +1,23 @@
+window.addEventListener("DOMContentLoaded", () => {
 
-const websocket = new WebSocket("ws://192.168.1.18:5678/");
 
-websocket.onopen = function() {
-  alert('Connected!')
-};
+  const socket = new WebSocket("ws://localhost:5678/");
 
-connection.addEventListener('message', function(event) {
+  // Connection opened
+  socket.onopen = function() {
+    console.log("Open.")
+  }
 
-websocket.onmessage = function(data) {
-  document.getElementById("idInput").value = data;
-  document.getElementById('scanForm').submit();
-};
+  // const messages = document.createElement("ul");
+  // document.body.appendChild(messages);
+  socket.onmessage = function(event) {
+    console.log("Message.");
+    document.getElementById("idInput").value = event.data;
+    // document.getElementById('scanForm').submit();
+    // const message = document.createElement("li");
+    // const content = document.createTextNode(event);
+    // message.appendChild(content);
+    // messages.appendChild(message);
+  }
 
 });
-// eventName : open, close, send, message...
-
-
