e70b821629764e84bbc139ae2049663040c8fe74
HoangHiep <nhoanghiep2001@gmail.com>
2023-08-12 15:44:05 +0700

    HTML CSS modified

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -126,9 +126,7 @@ USE_TZ = True
 STATIC_URL = 'Product/static/'
 
 STATICFILES_DIRS = [
-            os.path.join(BASE_DIR,'static'),
             os.path.join(BASE_DIR,'Product/static'),
-            os.path.join(BASE_DIR,'product/Product/static'),
 ]
 
 # Default primary key field type
